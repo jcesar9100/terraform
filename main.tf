afe478db11b2cf85390a6a69ca0ade215343a5e9
root <jcesar9100@gmail.com>
2025-04-02 13:48:21 -0300

    Alteração no EC2

--- main.tf
+++ main.tf
@@ -8,11 +8,42 @@ provider "aws" {
 resource "aws_instance" "exemple_server" {
   ami           = "ami-0d866da98d63e2b42"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  
+  user_data = <<-EOF
+  #!/bin/bash
+              sudo apt update -y
+              sudo apt install apache2 -y
+              sudo systemctl start apache2
+              sudo bash -c 'echo Hello World > /var/www/html/index.html'
+              EOF
+  
+  user_data_replace_on_change = true
 
   tags = {
     Name= "Ubuntu_server"
   }
 
+}
+
+resource "aws_security_group" "instance" {
+	name = "teste_servidor"
+
+	//porta de entrada
+	  ingress { 
+		from_port = 80
+		to_port = 80
+		protocol = "tcp"
+  	cidr_blocks = ["0.0.0.0/0"]
+	}
+  
+    egress {
+        from_port       = 0
+        to_port         = 0
+        protocol        = "-1"
+        cidr_blocks     = ["0.0.0.0/0"]
+  }
+  
 }
 
 
