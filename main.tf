f193a808114ecfc78f95f7f4c218933d879d4147
root <jcesar9100@gmail.com>
2025-04-02 14:20:09 -0300

    Atualização de variavel de saida

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ provider "aws" {
     
 }
 
+variable "server_port"{
+    description= "Porta de Serviço Web"
+    type = number
+    default = 80
+
+}
+
 resource "aws_instance" "exemple_server" {
   ami           = "ami-0d866da98d63e2b42"
   instance_type = "t2.micro"
@@ -15,7 +22,7 @@ resource "aws_instance" "exemple_server" {
               sudo apt update -y
               sudo apt install apache2 -y
               sudo systemctl start apache2
-              sudo bash -c 'echo Hello World > /var/www/html/index.html'
+              sudo bash -c 'echo Hello World > run on port ${var.server_port}>/var/www/html/index.html'
               EOF
   
   user_data_replace_on_change = true
@@ -27,12 +34,12 @@ resource "aws_instance" "exemple_server" {
 }
 
 resource "aws_security_group" "instance" {
-	name = "teste_servidor"
+	name = "Ubuntu_server_Node1"
 
 	//porta de entrada
 	  ingress { 
-		from_port = 80
-		to_port = 80
+		from_port = var.server_port
+		to_port = var.server_port
 		protocol = "tcp"
   	cidr_blocks = ["0.0.0.0/0"]
 	}
@@ -43,6 +50,12 @@ resource "aws_security_group" "instance" {
         protocol        = "-1"
         cidr_blocks     = ["0.0.0.0/0"]
   }
+  
+}
+
+output "public_ip" {
+  value = aws_instance.exemple_server.public_ip
+  description = "Ip Publico da Instancia EC2"
   
 }
 
